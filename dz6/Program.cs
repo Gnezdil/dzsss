79763e0197c3152cb2d8965f1ea15cb354d68d54
DGnezdilOFF <120dimos@mail.ru>
2023-03-08 14:58:45 +0300

    +dz6

---
+++ dz6/Program.cs
@@ -0,0 +1,11 @@
+﻿// напишите программу которая на вход принимает число и показывет 
+// являеться ли оно четным ( делиться ли оно на 2 ).
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("ess");
+}
+else
+{
+    Console.WriteLine("no");
+}
