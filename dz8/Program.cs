cba8190dcdfa50eaded8f6f9596f4de908ef533f
DGnezdilOFF <120dimos@mail.ru>
2023-03-08 15:00:45 +0300

    +dz8

---
+++ dz8/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите программу которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N.
+// 8 -> 2,4,6,8.
+
+Console.WriteLine("Введите длинну: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        System.Console.Write("Результат: ");
+        Console.WriteLine(i);
+    }
+}
