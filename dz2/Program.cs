b3b6dafbbccc26701c08de9df19904d1f9951700
DGnezdilOFF <120dimos@mail.ru>
2023-03-08 14:47:57 +0300

    +dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// напишите команду которая на вход пинимает два числа 
+// и показывает какое из них большее а какое меньшее .
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("nunber 1 max");
+    Console.WriteLine("number 2 min");
+}
+if (a < b)
+{
+    Console.WriteLine("number 1 min");
+    Console.WriteLine("number 2 max");
+}
